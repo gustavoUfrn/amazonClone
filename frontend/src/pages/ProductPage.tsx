3dbeb27e50fd4098f4af3660b6279b670bf8390a
gustavoUfrn <mxgugaa@gmail.com>
2024-04-29 12:54:23 -0300

    .

--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -5,6 +5,8 @@ import MessageBox from '../components/MessageBox'
 import { ApiError } from '../types/ApiError'
 import { getError } from '../utils'
 import { useParams } from 'react-router-dom'
+import { Badge, Button, Card, Col, ListGroup, Row } from 'react-bootstrap'
+import Rating from '../components/Rating'
 
 export default function ProductPage() {
   const params = useParams()
@@ -25,9 +27,66 @@ export default function ProductPage() {
     <MessageBox variant="danger">Product Not Found!</MessageBox>
   ) : (
     <div>
-      <Helmet>
-        <title>Product Page</title>
-      </Helmet>
+      <Row>
+        <Col md={6}>
+          <img className="large" src={product.image} alt={product.name}></img>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <Helmet>
+                <title>{product.name}</title>
+              </Helmet>
+              <h1>{product.name}</h1>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                rating={product.rating}
+                numReviews={product.numReviews}
+              ></Rating>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <ListGroup.Item>Price : ${product.price}</ListGroup.Item>
+              <ListGroup.Item>
+                Description: <p>{product.description}</p>
+              </ListGroup.Item>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <Card.Body>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>${product.price}</Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? (
+                        <Badge bg="success">In Stock</Badge>
+                      ) : (
+                        <Badge bg="danger">Unavaliable</Badge>
+                      )}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <div className="d-grid">
+                      <Button variant="primary">Add to Cart</Button>
+                    </div>
+                  </ListGroup.Item>
+                )}
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </div>
   )
 }
